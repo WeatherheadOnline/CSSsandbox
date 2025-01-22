8536efdf225298d5cc0207d6b4f4805191b52878
WeatherheadOnline <WeatherheadOnline@gmail.com>
2025-01-22 12:15:01 -0700

    Adding new sections: picture frames, and two organic geometric backgrounds

--- js/peacock.js
+++ js/peacock.js
@@ -3,19 +3,79 @@ const cyan = document.getElementById("cyan");
 const blue = document.getElementById("blue");
 const magenta = document.getElementById("magenta");
 
-const senstitivity = 20;   // in vw units
+const sensitivity = 20;   // in vw units
 
 const setpointG = 40;   // in vw units
 const setpointC = 50;   // in vw units
 const setpointB = 60;   // in vw units
 const setpointM = 70;   // in vw units
 
-let screenXvw;  // this variable will hold e.screenX in vw units
+if ("ontouchstart" in window) {
+    mobileHandler();
+} else {
+    desktopListener();
+};
 
-document.addEventListener("mousemove", function(e) {
-    screenXvw = e.screenX / window.innerWidth * 100;  // get e.screenX in vw units
-    green.style.opacity = (senstitivity - Math.abs(screenXvw - setpointG))/senstitivity;
-    cyan.style.opacity = (senstitivity - Math.abs(screenXvw - setpointC))/senstitivity;
-    blue.style.opacity = (senstitivity- Math.abs(screenXvw - setpointB))/senstitivity;
-    magenta.style.opacity = (senstitivity - Math.abs(screenXvw - setpointM))/senstitivity;
-});
+function desktopListener() {
+    document.addEventListener("mousemove", function(e) {
+        let screenXvw = e.screenX / window.innerWidth * 100;  // get e.screenX in vw units
+        green.style.opacity = (sensitivity - Math.abs(screenXvw - setpointG))/sensitivity;
+        cyan.style.opacity = (sensitivity - Math.abs(screenXvw - setpointC))/sensitivity;
+        blue.style.opacity = (sensitivity- Math.abs(screenXvw - setpointB))/sensitivity;
+        magenta.style.opacity = (sensitivity - Math.abs(screenXvw - setpointM))/sensitivity;
+    });
+}
+
+function mobileHandler() {
+    portraitListener();
+    let portrait = window.matchMedia("(orientation: portrait)");
+    portrait.addEventListener("change", function(e) {
+        if(e.matches) {
+            portraitListener();
+        } else {
+            landscapeListener();
+        }
+    })
+}
+
+function portraitListener() {
+    window.addEventListener("deviceorientation", function(e){
+        let opacityG = (sensitivity - Math.abs(e.gamma - setpointG + 2*sensitivity)) / sensitivity;
+        if (opacityG < 0) {opacityG = 0}; 
+        green.style.opacity = opacityG;
+
+        let opacityC = (sensitivity - Math.abs(e.gamma - setpointC + 2*sensitivity)) / sensitivity;
+        if (opacityC < 0) {opacityC = 0}; 
+        cyan.style.opacity = opacityC;
+
+        let opacityB = (sensitivity - Math.abs(e.gamma - setpointB + 2*sensitivity)) / sensitivity;
+        if (opacityB < 0) {opacityB = 0}; 
+        blue.style.opacity = opacityB;
+
+        let opacityM = (sensitivity - Math.abs(e.gamma - setpointM + 2*sensitivity)) / sensitivity;
+        if (opacityM < 0) {opacityM = 0}; 
+        magenta.style.opacity = opacityM;
+    });
+}
+
+function landscapeListener() {
+    window.addEventListener("deviceorientation", function(e){
+        let opacityG = (sensitivity - Math.abs(e.beta - setpointG + 2*sensitivity)) / sensitivity;
+        if (opacityG < 0) {opacityG = 0}; 
+        green.style.opacity = opacityG;
+
+        let opacityC = (sensitivity - Math.abs(e.beta - setpointC + 2*sensitivity)) / sensitivity;
+        if (opacityC < 0) {opacityC = 0}; 
+        cyan.style.opacity = opacityC;
+
+        let opacityB = (sensitivity - Math.abs(e.beta - setpointB + 2*sensitivity)) / sensitivity;
+        if (opacityB < 0) {opacityB = 0}; 
+        blue.style.opacity = opacityB;
+
+        let opacityM = (sensitivity - Math.abs(e.beta - setpointM + 2*sensitivity)) / sensitivity;
+        if (opacityM < 0) {opacityM = 0}; 
+        magenta.style.opacity = opacityM;
+    });
+}
+
+
--- js/stairs.js
+++ js/stairs.js
@@ -1,41 +1,58 @@
 const container = document.getElementById("stairsContainer");
 const lineWidth = 8;  // Remember to update --line-reverse in styles.css under #stairs
-const screenWidth = screen.width + lineWidth;
-const screenHeight = screen.height + lineWidth;
-
 const unitWidth = 100;    // Remember to update --dimension in styles.css under #stairs
 const unitHeight = unitWidth;
+
+let screenWidth;
+let screenHeight;
 let measureWidth = 0;
 let measureHeight = 0;
 let countWidth = 0;
 let countHeight = 0;
 
-while (measureWidth <= screenWidth) {
-    measureWidth += unitWidth;
-    countWidth ++;
+function makeStairs() {
+    calculateDimensions();
+    drawStairs();
 }
 
-while (measureHeight <= (screenHeight)) {
-    measureHeight += unitHeight;
-    countHeight ++;
+makeStairs();
+
+// If mobile device changes orientation, re-calculate the background:
+screen.orientation.addEventListener("change", makeStairs);
+
+function calculateDimensions() {
+    screenWidth = screen.width + lineWidth;
+    screenHeight = screen.height + lineWidth;
+    while (measureWidth <= screenWidth) {
+        measureWidth += unitWidth;
+        countWidth ++;
+    };
+    while (measureHeight <= (screenHeight)) {
+        measureHeight += unitHeight;
+        countHeight ++;
+    };
+    container.style.width = `${measureWidth}px`
+    container.style.height = `${measureHeight}px`
 }
 
-container.style.width = `${measureWidth}px`
-container.style.height = `${measureHeight}px`
-
-let i = 0;
-while (i <= countWidth * countHeight) {
-    let stair = document.createElement("div");
-    stair.className = "stair";
-    let stairInner = document.createElement("div");
-    stairInner.className = "stairInner";
-    stair.appendChild(stairInner);
-    container.appendChild(stair);
-    let box = document.createElement("div");
-    box.className = "stairBox";
-    stair.appendChild(box);
-    i++;
+function drawStairs() {
+    let i = 0;
+    while (i <= countWidth * countHeight) {
+        let stair = document.createElement("div");
+        stair.className = "stair";
+        let stairInner = document.createElement("div");
+        stairInner.className = "stairInner";
+        stair.appendChild(stairInner);
+        container.appendChild(stair);
+        let box = document.createElement("div");
+        box.className = "stairBox";
+        stair.appendChild(box);
+        i++;
+    }
 }
+
+
+//  Enable button to toggle animation:
 
 const boxes = document.getElementsByClassName("stairBox");
 let toggle = true; 
@@ -46,13 +63,11 @@ function toggleAnimation() {
         for (let i = 0; i < boxes.length; i++) {
             boxes[i].style.animation = "none";
         }
-        // document.getElementsByClassName("stairBox").style.animation = "none";
         toggle = false;
     } else {
         for (let i = 0; i < boxes.length; i++) {
             boxes[i].style.animation = "stairStrobe 1s linear infinite";
         }
-        // document.getElementsByClassName("stairBox").style.animation = "stairStrobe 1s linear infinite";
         toggle = true;
     }
 }
