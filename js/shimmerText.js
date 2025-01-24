a0eb04e002aca479b1a0d9646a9fe60b076bcab8
WeatherheadOnline <WeatherheadOnline@gmail.com>
2025-01-24 13:52:28 -0700

    Added shimmering text section

---
+++ js/shimmerText.js
@@ -0,0 +1,22 @@
+console.clear();
+console.log("Initialized");
+
+const bkgd = document.getElementById("TextShimmer").children[0].children[2];
+
+if ("ontouchstart" in window) {
+    mobileListener();
+} else {
+    desktopListener();
+};
+
+function desktopListener() {
+    document.addEventListener("mousemove", function(e) {
+        bkgd.style.backgroundPosition = `${e.screenX*4 - window.innerWidth*4}px 0px`;
+    });
+};
+
+function mobileListener() {
+    window.addEventListener("deviceorientation", function(event){
+            bkgd.style.backgroundPosition = `${16 * event.gamma}px, 0px`;
+    });
+}
